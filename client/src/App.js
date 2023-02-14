b9770937b10526377c50097b6d3f292fdff71f33
Elankumaran98 <siva98kumarane@gmail.com>
2023-02-14 09:35:58 +0530

    bug fixed and pages created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import "./App.css"
 import Home from "./components/Home";
+import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Error from "./components/Error";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-    <Home/>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/menu" element={<Menu/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+      </Router>
     </>
   );
 };
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import eye from "../assests/eye.jpg"
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link to='/' className="navbar-brand" href="1">
+        <Link to="/" className="navbar-brand">
           <img
             src={eye}
             alt=""
@@ -15,17 +15,17 @@ const Navbar = () => {
             className="d-inline-block align-text-top rounded-pill me-3"
           />
           EL
-        </Link>
+        </Link >
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to='/' className="nav-link active" aria-current="page" href="1">
-                Home
+              <Link to="/menu" className="nav-link active" aria-current="page">
+                Menu
               </Link>
             </li>
             <li className="nav-item">
-              <Link to='/' className="nav-link" href="1">
-                Link
+              <Link to='/about' className="nav-link" >
+                About
               </Link>
             </li>
           </ul>
