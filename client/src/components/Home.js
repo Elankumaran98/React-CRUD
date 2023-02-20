3ec08477bc12b8ad32621dfa26654c6863bca825
Elankumaran98 <siva98kumarane@gmail.com>
2023-02-20 10:39:19 +0530

    get data

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,14 @@
 import { useEffect} from 'react'
+import axios from "axios"
 
 const Home = () => {
+  useEffect(() => {
+    loadData();
+  }, [])
+  const loadData=async() => {
+    const response=await axios.get('http://localhost:3003/users')
+    console.log(response.data)
+  }
   return (
     <div className='container'>
         Home
