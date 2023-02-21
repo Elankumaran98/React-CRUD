1151dd90a1448967990df465d83c32694f0d4de6
Elankumaran98 <siva98kumarane@gmail.com>
2023-02-21 09:02:47 +0530

    updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,19 +1,25 @@
-import { useEffect} from 'react'
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     loadData();
-  }, [])
-  const loadData=async() => {
-    const response=await axios.get('http://localhost:3003/users')
-    console.log(response.data)
-  }
+  }, []);
+  const loadData = async () => {
+    const response = await axios.get("http://localhost:3003/users");
+    console.log(response.data);
+    setUsers(response.data);
+  };
   return (
-    <div className='container'>
-        Home
+    <div className="container">
+      {users.map((user) => (
+        <div>
+          {user.id} {user.name}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
